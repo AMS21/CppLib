5b5e540709e0cd9578b0fda8a622991c6460c220
AMS21 <hansfritzenmeister@yahoo.de>
2019-02-16 18:15:31 +0100

    Suppress unused variable warning on GCC

--- test/src/AnonymousName_test.cpp
+++ test/src/AnonymousName_test.cpp
@@ -6,10 +6,15 @@ CPP_MSVC_SUPPRESS_WARNING_PUSH
 CPP_MSVC_SUPPRESS_WARNING(4189) // 'x': local variable is initialized but not referenced
 CPP_MSVC_SUPPRESS_WARNING(4101) // 'x': unreferenced local variable
 
+CPP_GCC_SUPPRESS_WARNING_PUSH
+CPP_GCC_SUPPRESS_WARNING("-Wunused-variable")
+
 TEST_CASE("anonymous name")
 {
     int CPP_ANONYMOUS_NAME(a);
     int CPP_ANONYMOUS_NAME(a);
 }
+
+CPP_GCC_SUPPRESS_WARNING_POP
 
 CPP_MSVC_SUPPRESS_WARNING_POP
