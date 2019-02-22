e847b88921602f3c4a7aeedbcfacc86ae6b20783
Unknown <hansfritzenmeister@yahoo.de>
2019-02-22 20:59:10 +0100

    Fixed warning in doctest

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,2 +1,9 @@
+#include "cpp/Warning.hpp"
+
+CPP_GCC_SUPPRESS_WARNING_PUSH
+CPP_GCC_SUPPRESS_WARNING("-Wsuggest-attribute=noreturn")
+
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+
+CPP_GCC_SUPPRESS_WARNING_POP
