34bd1ecfbce82d10c12f7d46fa149e991596659e
Unknown <hansfritzenmeister@yahoo.de>
2019-02-22 20:41:57 +0100

    Fixed test case on clang

--- test/src/AnonymousName_test.cpp
+++ test/src/AnonymousName_test.cpp
@@ -2,6 +2,12 @@
 #include "cpp/Warning.hpp"
 #include "doctest.h"
 
+class CPP_ANONYMOUS_NAME(A)
+{};
+
+class CPP_ANONYMOUS_NAME(A)
+{};
+
 CPP_MSVC_SUPPRESS_WARNING_PUSH
 CPP_MSVC_SUPPRESS_WARNING(4189) // 'x': local variable is initialized but not referenced
 CPP_MSVC_SUPPRESS_WARNING(4101) // 'x': unreferenced local variable
@@ -9,11 +15,16 @@ CPP_MSVC_SUPPRESS_WARNING(4101) // 'x': unreferenced local variable
 CPP_GCC_SUPPRESS_WARNING_PUSH
 CPP_GCC_SUPPRESS_WARNING("-Wunused-variable")
 
+CPP_CLANG_SUPPRESS_WARNING_PUSH
+CPP_CLANG_SUPPRESS_WARNING("-Wunused-variable")
+
 TEST_CASE("anonymous name")
 {
     int CPP_ANONYMOUS_NAME(a);
     int CPP_ANONYMOUS_NAME(a);
 }
+
+CPP_CLANG_SUPPRESS_WARNING_POP
 
 CPP_GCC_SUPPRESS_WARNING_POP
 
