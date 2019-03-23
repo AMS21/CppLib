a9a2b3d6e2070a3fe7297d1375c0ed830cc380e4
AMS21 <hansfritzenmeister@yahoo.de>
2019-03-23 23:58:02 +0100

    Added NamedType

---
+++ include/cpp/NamedType.hpp
@@ -0,0 +1,241 @@
+#pragma once
+
+// Based on the excelent work from Jonathan Boccara on his NamedType library: https://github.com/joboccara/NamedType
+
+// List of changes:
+// Constructor is noexcept if constructing T is noexcept
+// noexcept on get
+// NamedType is default constructible if T is default constructible
+// Added PostIncrementable
+// Added Divisible
+
+#include <iostream>    // std::ostream
+#include <memory>      // std::addressof
+#include <type_traits> // std::remove_reference_t, std::enable_if, std::remove_reference_t
+#include <utility>     // std::move, std::forward
+
+// Enable empty base class optimization with multiple inheritance on Visual Studio.
+#if defined(_MSC_VER) && _MSC_VER >= 1910
+#    define CPP_EBCO __declspec(empty_bases)
+#else
+#    define CPP_EBCO
+#endif
+
+namespace cpp
+{
+    namespace detail
+    {
+        template <typename T, template <typename> class crtpType>
+        struct crtp
+        {
+            T&       underlying() { return static_cast<T&>(*this); }
+            const T& underlying() const { return static_cast<const T&>(*this); }
+        };
+
+        template <typename T>
+        using IsNotReference = typename std::enable_if<!std::is_reference<T>::value, void>::type;
+    } // namespace detail
+
+    template <typename T, typename Parameter, template <typename> class... Skills>
+    class CPP_EBCO NamedType : public Skills<NamedType<T, Parameter, Skills...>>...
+    {
+    public:
+        using type = T;
+
+        // constructor
+        template <typename T_ = T, typename = std::enable_if<std::is_default_constructible_v<T>, void>>
+        constexpr NamedType() noexcept(noexcept(T()))
+        {}
+
+        explicit constexpr NamedType(const T& value) noexcept(noexcept(T(value)))
+            : mValue(value)
+        {}
+
+        template <typename T_ = T, typename = detail::IsNotReference<T_>>
+        explicit constexpr NamedType(T&& value) noexcept(noexcept(T(std::move(value))))
+            : mValue(std::move(value))
+        {}
+
+        // get
+        constexpr T& get() noexcept { return mValue; }
+
+        constexpr const std::remove_reference_t<T>& get() const noexcept { return mValue; }
+
+        // conversions
+        using ref = NamedType<T&, Parameter, Skills...>;
+        operator ref() { return ref(mValue); }
+
+        struct argument
+        {
+            template <typename U>
+            NamedType operator=(U&& value) const
+            {
+                return NamedType(std::forward<U>(value));
+            }
+            argument()                = default;
+            argument(argument const&) = delete;
+            argument(argument&&)      = delete;
+            argument& operator=(argument const&) = delete;
+            argument& operator=(argument&&) = delete;
+        };
+
+    private:
+        T mValue;
+    };
+
+    template <template <typename T> class StrongType, typename T>
+    constexpr StrongType<T> make_named(const T& value)
+    {
+        return StrongType<T>(value);
+    }
+
+    //
+
+    template <typename T>
+    struct Incrementable : detail::crtp<T, Incrementable>
+    {
+        T& operator+=(T const& other)
+        {
+            this->underlying().get() += other.get();
+            return this->underlying();
+        }
+    };
+
+    template <typename T>
+    struct PreIncrementable : detail::crtp<T, PreIncrementable>
+    {
+        T& operator++()
+        {
+            ++this->underlying().get();
+            return this->underlying();
+        }
+    };
+
+    template <typename T>
+    struct PostIncrementable : detail::crtp<T, PostIncrementable>
+    {
+        T operator++(int) { return this->underlying().get()++; }
+    };
+
+    template <typename T>
+    struct Addable : detail::crtp<T, Addable>
+    {
+        T operator+(const T& other) const { return T(this->underlying().get() + other.get()); }
+    };
+
+    template <typename T>
+    struct Subtractable : detail::crtp<T, Subtractable>
+    {
+        T operator-(const T& other) const { return T(this->underlying().get() - other.get()); }
+    };
+
+    template <typename T>
+    struct Multiplicable : detail::crtp<T, Multiplicable>
+    {
+        T operator*(const T& other) const { return T(this->underlying().get() * other.get()); }
+    };
+
+    template <typename T>
+    struct Divisible : detail::crtp<T, Divisible>
+    {
+        T operator/(const T& other) const { return T(this->underlying().get() / other.get()); }
+    };
+
+    template <typename T>
+    struct Negatable : detail::crtp<T, Negatable>
+    {
+        T operator-() const { return T(-this->underlying().get()); }
+    };
+
+    template <typename T>
+    struct Comparable : detail::crtp<T, Comparable>
+    {
+        bool operator<(T const& other) const { return this->underlying().get() < other.get(); }
+        bool operator>(T const& other) const { return other.get() < this->underlying().get(); }
+        bool operator<=(T const& other) const { return !(other.get() < this->underlying().get()); }
+        bool operator>=(T const& other) const { return !(*this < other); }
+        bool operator==(T const& other) const { return !(*this < other) && !(other.get() < this->underlying().get()); }
+        bool operator!=(T const& other) const { return !(*this == other); }
+    };
+
+    template <typename T>
+    struct Printable : detail::crtp<T, Printable>
+    {
+        void print(std::ostream& os) const { os << this->underlying().get(); }
+    };
+
+    template <typename T, typename Parameter, template <typename> class... Skills>
+    std::ostream& operator<<(std::ostream& os, const NamedType<T, Parameter, Skills...>& object)
+    {
+        object.print(os);
+        return os;
+    }
+
+    template <typename Destination>
+    struct ImplicitlyConvertibleTo
+    {
+        template <typename T>
+        struct templ : detail::crtp<T, templ>
+        {
+            operator Destination() const { return this->underlying().get(); }
+        };
+    };
+
+    template <typename T>
+    struct Hashable
+    {
+        static constexpr bool is_hashable = true;
+    };
+
+    template <typename NamedType_>
+    struct FunctionCallable;
+
+    template <typename T, typename Parameter, template <typename> class... Skills>
+    struct FunctionCallable<NamedType<T, Parameter, Skills...>>
+        : detail::crtp<NamedType<T, Parameter, Skills...>, FunctionCallable>
+    {
+        operator T&() { return this->underlying().get(); }
+        operator const T&() const { return this->underlying().get(); }
+    };
+
+    template <typename NamedType_>
+    struct MethodCallable;
+
+    template <typename T, typename Parameter, template <typename> class... Skills>
+    struct MethodCallable<NamedType<T, Parameter, Skills...>>
+        : detail::crtp<NamedType<T, Parameter, Skills...>, MethodCallable>
+    {
+        T*       operator->() { return std::addressof(this->underlying().get()); }
+        T const* operator->() const { return std::addressof(this->underlying().get()); }
+    };
+
+    template <typename NamedType_>
+    struct Callable : FunctionCallable<NamedType_>, MethodCallable<NamedType_>
+    {};
+
+    template <typename T>
+    struct Arithmetic : public Incrementable<T>,
+                        PreIncrementable<T>,
+                        PostIncrementable<T>,
+                        Addable<T>,
+                        Subtractable<T>,
+                        Multiplicable<T>,
+                        Divisible<T>,
+                        Negatable<T>,
+                        Comparable<T>,
+                        Printable<T>,
+                        Hashable<T>
+    {};
+} // namespace cpp
+
+namespace std
+{
+    template <typename T, typename Parameter, template <typename> class... Skills>
+    struct hash<cpp::NamedType<T, Parameter, Skills...>>
+    {
+        using NamedType       = cpp::NamedType<T, Parameter, Skills...>;
+        using checkIfHashable = typename std::enable_if<NamedType::is_hashable, void>::type;
+
+        size_t operator()(const cpp::NamedType<T, Parameter, Skills...>& x) const { return std::hash<T>()(x.get()); }
+    };
+} // namespace std
---
+++ test/src/NamedType_test.cpp
@@ -0,0 +1,368 @@
+#include "cpp/NamedType.hpp"
+#include "doctest.h"
+
+#include "cpp/Warning.hpp"
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+
+template <typename T>
+decltype(auto) tee(T&& value)
+{
+    std::cout << value << '\n';
+    return std::forward<T>(value);
+}
+
+using Meter = cpp::NamedType<double, struct MeterParameter, cpp::Addable, cpp::Comparable>;
+Meter operator"" _meter(unsigned long long value) { return Meter(static_cast<double>(value)); }
+//Meter operator"" _meter(long double value) { return Meter(value); }
+
+using Width  = cpp::NamedType<Meter, struct WidthParameter>;
+using Height = cpp::NamedType<Meter, struct HeightParameter>;
+
+class Rectangle
+{
+public:
+    Rectangle(Width width, Height height)
+        : width_(width.get())
+        , height_(height.get())
+    {}
+    Meter getWidth() const { return width_; }
+    Meter getHeight() const { return height_; }
+
+private:
+    Meter width_;
+    Meter height_;
+};
+
+TEST_CASE("Basic usage")
+{
+    Rectangle r(Width(10_meter), Height(12_meter));
+    CHECK_EQ(r.getWidth().get(), 10);
+    CHECK_EQ(r.getHeight().get(), 12);
+}
+
+using NameRef = cpp::NamedType<std::string&, struct NameRefParameter>;
+
+void changeValue(NameRef name) { name.get() = "value2"; }
+
+TEST_CASE("Passing a strong reference")
+{
+    std::string value = "value1";
+    changeValue(NameRef(value));
+    CHECK_EQ(value, "value2");
+}
+
+TEST_CASE("Construction of NamedType::ref from the underlying type")
+{
+    using StrongInt = cpp::NamedType<int, struct StrongIntTag>;
+    auto addOne     = [](StrongInt::ref si) { ++(si.get()); };
+
+    int i = 42;
+    addOne(StrongInt::ref(i));
+    CHECK_EQ(i, 43);
+}
+
+TEST_CASE("Implicit conversion of NamedType to NamedType::ref")
+{
+    using StrongInt = cpp::NamedType<int, struct StrongIntTag>;
+    auto addOne     = [](StrongInt::ref si) { ++(si.get()); };
+
+    StrongInt i(42);
+    addOne(i);
+    CHECK_EQ(i.get(), 43);
+
+    StrongInt j(42);
+    addOne(StrongInt::ref(j));
+    CHECK_EQ(j.get(), 43);
+}
+
+template <typename Function>
+using Comparator = cpp::NamedType<Function, struct ComparatorParameter>;
+
+template <typename Function>
+std::string performAction(Comparator<Function> comp)
+{
+    return comp.get()();
+}
+
+TEST_CASE("Strong generic type")
+{
+    REQUIRE(performAction(cpp::make_named<Comparator>([]() { return std::string("compare"); })) == "compare");
+}
+
+TEST_CASE("Addable")
+{
+    using AddableType = cpp::NamedType<int, struct AddableTag, cpp::Addable>;
+    AddableType s1(12);
+    AddableType s2(10);
+    CHECK_EQ((s1 + s2).get(), 22);
+}
+
+TEST_CASE("Subtractable")
+{
+    using SubtractableType = cpp::NamedType<int, struct SubtractableTag, cpp::Subtractable>;
+    SubtractableType s1(12);
+    SubtractableType s2(10);
+    CHECK_EQ((s1 - s2).get(), 2);
+}
+
+TEST_CASE("Multiplicable")
+{
+    using MultiplicableType = cpp::NamedType<int, struct MultiplicableTag, cpp::Multiplicable>;
+    MultiplicableType s1(12);
+    MultiplicableType s2(10);
+    CHECK_EQ((s1 * s2).get(), 120);
+}
+
+TEST_CASE("Divisible")
+{
+    using DivisibleType = cpp::NamedType<int, struct DivisibleTag, cpp::Divisible>;
+    DivisibleType s1(120);
+    DivisibleType s2(10);
+    CHECK_EQ((s1 / s2).get(), 12);
+}
+
+TEST_CASE("Negatable")
+{
+    using NegatableType = cpp::NamedType<int, struct NegatableTag, cpp::Negatable>;
+    NegatableType value(10);
+    CHECK_EQ((-value).get(), -10);
+}
+
+TEST_CASE("Comparable")
+{
+    CHECK((10_meter == 10_meter));
+    CHECK(!(10_meter == 11_meter));
+    CHECK((10_meter != 11_meter));
+    CHECK(!(10_meter != 10_meter));
+    CHECK((10_meter < 11_meter));
+    CHECK(!(10_meter < 10_meter));
+    CHECK((10_meter <= 10_meter));
+    CHECK((10_meter <= 11_meter));
+    CHECK(!(10_meter <= 9_meter));
+    CHECK((11_meter > 10_meter));
+    CHECK(!(10_meter > 11_meter));
+    CHECK((11_meter >= 10_meter));
+    CHECK((10_meter >= 10_meter));
+    CHECK(!(9_meter >= 10_meter));
+}
+
+TEST_CASE("ConvertibleWithOperator")
+{
+    struct B
+    {
+        B(int x)
+            : x(x)
+        {}
+        int x;
+    };
+
+    struct A
+    {
+        A(int x)
+            : x(x)
+        {}
+            operator B() const { return B(x); }
+        int x;
+    };
+
+    using StrongA = cpp::NamedType<A, struct StrongATag, cpp::ImplicitlyConvertibleTo<B>::templ>;
+    StrongA strongA(A(42));
+    B       b = strongA;
+    CHECK_EQ(b.x, 42);
+}
+
+TEST_CASE("ConvertibleWithConstructor")
+{
+    struct A
+    {
+        A(int x)
+            : x(x)
+        {}
+        int x;
+    };
+
+    struct B
+    {
+        B(A a)
+            : x(a.x)
+        {}
+        int x;
+    };
+
+    using StrongA = cpp::NamedType<A, struct StrongATag, cpp::ImplicitlyConvertibleTo<B>::templ>;
+    StrongA strongA(A(42));
+    B       b = strongA;
+    CHECK_EQ(b.x, 42);
+}
+
+TEST_CASE("ConvertibleToItself")
+{
+    using MyInt = cpp::NamedType<int, struct MyIntTag, cpp::ImplicitlyConvertibleTo<int>::templ>;
+    MyInt myInt(42);
+    int   i = myInt;
+    CHECK_EQ(i, 42);
+}
+
+TEST_CASE("Hash")
+{
+    using SerialNumber = cpp::NamedType<std::string, struct SerialNumberTag, cpp::Comparable, cpp::Hashable>;
+
+    std::unordered_map<SerialNumber, int> hashMap = {{SerialNumber{"AA11"}, 10}, {SerialNumber{"BB22"}, 20}};
+    SerialNumber                          cc33{"CC33"};
+    hashMap[cc33] = 30;
+    CHECK_EQ(hashMap[SerialNumber{"AA11"}], 10);
+    CHECK_EQ(hashMap[SerialNumber{"BB22"}], 20);
+    CHECK_EQ(hashMap[cc33], 30);
+}
+
+struct testFunctionCallable_A
+{
+    testFunctionCallable_A(int x)
+        : x(x)
+    {}
+    testFunctionCallable_A(testFunctionCallable_A const&) =
+            delete; // ensures that passing the argument to a function doesn't make a copy
+    testFunctionCallable_A(testFunctionCallable_A&&) = default;
+    testFunctionCallable_A& operator+=(testFunctionCallable_A const& other)
+    {
+        x += other.x;
+        return *this;
+    }
+    int x;
+};
+
+testFunctionCallable_A operator+(testFunctionCallable_A const& a1, testFunctionCallable_A const& a2)
+{
+    return testFunctionCallable_A(a1.x + a2.x);
+}
+
+bool operator==(testFunctionCallable_A const& a1, testFunctionCallable_A const& a2) { return a1.x == a2.x; }
+
+TEST_CASE("Function callable")
+{
+    using A              = testFunctionCallable_A;
+    auto functionTakingA = [](A const& a) { return a.x; };
+
+    using StrongA = cpp::NamedType<A, struct StrongATag, cpp::FunctionCallable>;
+    StrongA       strongA(A(42));
+    const StrongA constStrongA(A(42));
+    CHECK_EQ(functionTakingA(strongA), 42);
+    CHECK_EQ(functionTakingA(constStrongA), 42);
+    CHECK_EQ(strongA + strongA, 84);
+}
+
+TEST_CASE("Method callable")
+{
+    class A
+    {
+    public:
+        A(int x)
+            : x(x)
+        {}
+        A(A const&) = delete; // ensures that invoking a method doesn't make a copy
+        A(A&&)      = default;
+
+        int method() { return x; }
+        int constMethod() const { return x; }
+
+    private:
+        int x;
+    };
+
+    using StrongA = cpp::NamedType<A, struct StrongATag, cpp::MethodCallable>;
+    StrongA       strongA(A(42));
+    const StrongA constStrongA(A((42)));
+    CHECK_EQ(strongA->method(), 42);
+    CHECK_EQ(constStrongA->constMethod(), 42);
+}
+
+TEST_CASE("Callable")
+{
+    class A
+    {
+    public:
+        A(int x)
+            : x(x)
+        {}
+        A(A const&) = delete; // ensures that invoking a method or function doesn't make a copy
+        A(A&&)      = default;
+
+        int method() { return x; }
+        int constMethod() const { return x; }
+
+    private:
+        int x;
+    };
+
+    auto functionTakingA = [](A const& a) { return a.constMethod(); };
+
+    using StrongA = cpp::NamedType<A, struct StrongATag, cpp::Callable>;
+    StrongA       strongA(A(42));
+    const StrongA constStrongA(A((42)));
+    REQUIRE(functionTakingA(strongA) == 42);
+    REQUIRE(strongA->method() == 42);
+    REQUIRE(constStrongA->constMethod() == 42);
+}
+
+TEST_CASE("Named arguments")
+{
+    using FirstName = cpp::NamedType<std::string, struct FirstNameTag>;
+    using LastName  = cpp::NamedType<std::string, struct LastNameTag>;
+    static const FirstName::argument firstName;
+    static const LastName::argument  lastName;
+    auto                             getFullName = [](const FirstName& firstName, const LastName& lastName) {
+        return firstName.get() + lastName.get();
+    };
+
+    auto fullName = getFullName(firstName = "James", lastName = "Bond");
+    REQUIRE(fullName == "JamesBond");
+}
+
+TEST_CASE("Empty base class optimization") { CHECK_EQ(sizeof(Meter), sizeof(Meter::type)); }
+
+using strong_float = cpp::NamedType<float, struct FloatTag>;
+
+TEST_CASE("Default constructible")
+{
+    strong_float f1;
+    strong_float f2{};
+}
+
+struct throw_on_construction
+{
+    throw_on_construction() { throw 42; }
+
+    throw_on_construction(int) { throw "exception"; }
+};
+
+using C = cpp::NamedType<throw_on_construction, struct throwTag>;
+
+TEST_CASE("noexcept")
+{
+    CHECK_UNARY(noexcept(strong_float{}));
+    CHECK_UNARY_FALSE(noexcept(C{}));
+
+    CHECK_UNARY(noexcept(strong_float(3.14f)));
+    CHECK_UNARY_FALSE(noexcept(C{5}));
+}
+
+using strong_bool = cpp::NamedType<bool, struct BoolTag>;
+
+TEST_CASE("constexpr") { static_assert(strong_bool{true}.get()); }
+
+TEST_CASE("Arithmetic")
+{
+    using strong_integer = cpp::NamedType<int, struct IntTag, cpp::Arithmetic>;
+    strong_integer a{1};
+    strong_integer b{2};
+
+    CHECK_EQ((a + b).get(), 3);
+    a += b;
+    CHECK_EQ(a.get(), 3);
+    CHECK_EQ((a - b).get(), 1);
+}
