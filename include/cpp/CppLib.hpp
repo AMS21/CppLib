116ac8f9ad3659117e77a1a00f2310f4a8b1016c
AMS21 <hansfritzenmeister@yahoo.de>
2019-03-27 12:37:59 +0100

    Added basic version stuff

--- include/cpp/CppLib.hpp
+++ include/cpp/CppLib.hpp
@@ -1,2 +1,10 @@
 #pragma once
-// TODO: Implement version and other stuff, now its only used to identify the lib using cmake
+
+#include "Stringify.hpp"
+
+#define CPP_VERSION_MAJOR 0
+#define CPP_VERSION_MINOR 1
+#define CPP_VERSION_PATCH 0
+
+#define CPP_VERSION_STR                                                                                                \
+    CPP_STRINGIFY(CPP_VERSION_MAJOR) "." CPP_STRINGIFY(CPP_VERSION_MINOR) "." CPP_STRINGIFY(CPP_VERSION_PATCH)
