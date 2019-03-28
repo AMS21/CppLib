b65d90d2f3fc41f5bba8ef9db0841ded98427c7c
AMS21 <hansfritzenmeister@yahoo.de>
2019-03-28 11:57:15 +0100

    Added doxygen documentation for BeginEndMacro.hpp

--- include/cpp/BeginEndMacro.hpp
+++ include/cpp/BeginEndMacro.hpp
@@ -1,7 +1,26 @@
+/*!
+ * \file BeginEndMacro.hpp
+ * \brief Exports macros that simplify the definition of convenient to use
+ *        function like macros.
+**/
 #pragma once
 
 // clang-format off
+
+/*!
+ * \def CPP_BEGIN_MACRO
+ * \brief To be used to begin definitions of macros that shall be forced to
+ *        require a semicolon when used.
+ * \see CPP_END_MACRO
+**/
 #define CPP_BEGIN_MACRO do {
 
+/*!
+ * \def CPP_END_MACRO
+ * \brief To be used to end definitions of macros that shall be forced to
+ *        require a semicolon when used.
+ * \see CPP_BEGIN_MACRO
+**/
 #define CPP_END_MACRO } while ((void) 0, 0) /* avoid warnings */
+
 // clang-format on
