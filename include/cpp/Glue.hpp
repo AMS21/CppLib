aac4bb80b504e09408fd36907db913283e637ad4
AMS21 <hansfritzenmeister@yahoo.de>
2019-03-28 11:53:35 +0100

    Added doxygen documentation for Glue.hpp

--- include/cpp/Glue.hpp
+++ include/cpp/Glue.hpp
@@ -1,5 +1,23 @@
+/*!
+ * \file Glue.hpp
+ * \brief Exports a macro that can be used to glue two sequences of tokens
+ *        together in order to form a single sequence of tokens at
+ *        preprocessing time.
+**/
 #pragma once
 
+/*!
+ * \def CPP_DETAIL_GLUE(a, b)
+ * \brief Implementation macro for CPP_GLUE
+ * \warning Do not use.
+ * \see CPP_GLUE
+**/
 #define CPP_DETAIL_GLUE(a, b) a##b
 
+/*!
+ * \def CPP_GLUE(a, b)
+ * \brief Token pastes two contiguous sequences of tokens together as one.
+ * \example CPP_GLUE(hello, world) results in:
+ *          helloworld
+**/
 #define CPP_GLUE(a, b) CPP_DETAIL_GLUE(a, b)
