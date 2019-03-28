5c3fa8594e0b3f44403b6c2bfb38e294770f37b3
AMS21 <hansfritzenmeister@yahoo.de>
2019-03-28 11:52:11 +0100

    Added doxygen documentation for Inline.hpp

--- include/cpp/Inline.hpp
+++ include/cpp/Inline.hpp
@@ -1,6 +1,24 @@
+/*!
+ * \file Inline.hpp
+ * \brief Exports macros to force or forbid inlining of a function.
+**/
 #pragma once
 
 #include "Compiler.hpp"
+
+/*!
+ * \def CPP_ALWAYS_INLINE
+ * \brief Declares a function as always to be inlined (if possible).
+ * \warning Overrides the compiler's cost-benefit-analysis in regards to
+ *          inlining. Only use when certain. May not work if compiler is
+ *          unknown.
+**/
+
+/*!
+ * \def CPP_NEVER_INLINE
+ * \brief Declares a function as never to be inlined.
+ * \warning May not work if compiler is unknown.
+**/
 
 #if CPP_COMPILER_IS(CPP_COMPILER_GCC)
 #    define CPP_ALWAYS_INLINE __attribute__((always_inline)) inline
