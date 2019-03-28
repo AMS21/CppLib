03c76f2e85971ada3b1c9214856a575aa4be73aa
AMS21 <hansfritzenmeister@yahoo.de>
2019-03-28 11:48:46 +0100

    doxygen documentation for Stringify.hpp

--- include/cpp/Stringify.hpp
+++ include/cpp/Stringify.hpp
@@ -1,5 +1,23 @@
+/*!
+ * \file Stringify.hpp
+ * \brief Exports a macro that can be used to turn any sequence of tokens
+ *        into a C-string literal at preprocessing time.
+**/
 #pragma once
 
+/*!
+ * \def CPP_DETAIL_STRINGIFY(tokens)
+ * \brief Implementation macro for CPP_STRINGIFY
+ * \warning Do not use.
+ * \see CPP_STRINGIFY
+**/
 #define CPP_DETAIL_STRINGIFY(tokens) #tokens
 
+/*!
+ * \def CPP_STRINGIFY(tokens)
+ * \brief Turns a contiguous sequence of tokens passed in into a
+ *        C-String literal.
+ * \example CPP_STRINGIFY(helloworld) results in:
+ *          "helloworld"
+**/
 #define CPP_STRINGIFY(tokens) CPP_DETAIL_STRINGIFY(tokens)
