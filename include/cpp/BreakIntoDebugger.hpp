5f9bf947c1da11f7bd71ac153c6ea2e723e36279
AMS21 <hansfritzenmeister@yahoo.de>
2019-03-31 22:56:36 +0200

    Documentation for BreakIntoDebugger

--- include/cpp/BreakIntoDebugger.hpp
+++ include/cpp/BreakIntoDebugger.hpp
@@ -1,7 +1,12 @@
+/*!
+ * \file BreakIntoDebugger.hpp
+ * \brief Cross platform macro to break into the debugger
+ *
+**/
 #pragma once
 
-#include "Compiler.hpp"
-#include "OS.hpp"
+#include "Compiler.hpp" // CPP_COMPILER_IS, CPP_COMPILER_MSVC, CPP_COMPILER_MINGW
+#include "OS.hpp"       // CPP_OS_IS, CPP_OS_MACOS
 
 #if CPP_OS_IS(CPP_OS_MACOS)
 #    define CPP_BREAK_INTO_DEBUGGER() __asm__("int $3\n" : :)
@@ -13,3 +18,10 @@ extern "C" __declspec(dllimport) void __stdcall DebugBreak();
 #else
 #    define CPP_BREAK_INTO_DEBUGGER() ((void)0)
 #endif
+
+/*!
+ * \def CPP_BREAK_INTO_DEBUGGER
+ * \brief Function like macro for breaking into the debugger
+ * \note Currently only supported on MacOs, MSVC and mingw and
+ *       all other platforms/compilers it does nothing
+**/
