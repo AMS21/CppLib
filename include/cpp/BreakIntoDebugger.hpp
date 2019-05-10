a6d3ae76b604f14f9ada8606c1db6629fd18ee3e
AMS21 <hansfritzenmeister@yahoo.de>
2019-05-10 16:31:49 +0200

    Add support for Linux using raise sigtrap

--- include/cpp/BreakIntoDebugger.hpp
+++ include/cpp/BreakIntoDebugger.hpp
@@ -6,7 +6,7 @@
 #pragma once
 
 #include "Compiler.hpp" // CPP_COMPILER_IS, CPP_COMPILER_MSVC, CPP_COMPILER_MINGW
-#include "OS.hpp"       // CPP_OS_IS, CPP_OS_MACOS
+#include "OS.hpp"       // CPP_OS_IS, CPP_OS_MACOS, CPP_OS_LINUX
 
 #if CPP_OS_IS(CPP_OS_MACOS)
 #    define CPP_BREAK_INTO_DEBUGGER() __asm__("int $3\n" : :)
@@ -15,6 +15,9 @@
 #elif CPP_COMPILER_IS(CPP_COMPILER_MINGW)
 extern "C" __declspec(dllimport) void __stdcall DebugBreak();
 #    define CPP_BREAK_INTO_DEBUGGER() ::DebugBreak()
+#elif CPP_OS_IS(CPP_OS_LINUX)
+#    include <signal.h>
+#    define CPP_BREAK_INTO_DEBUGGER() raise(SIGTRAP)
 #else
 #    define CPP_BREAK_INTO_DEBUGGER() ((void)0)
 #endif
@@ -22,6 +25,6 @@ extern "C" __declspec(dllimport) void __stdcall DebugBreak();
 /*!
  * \def CPP_BREAK_INTO_DEBUGGER
  * \brief Function like macro for breaking into the debugger
- * \note Currently only supported on MacOs, MSVC and MinGW on
- *       all other platforms/compilers it simply does nothing
+ * \note Currently only supported on MacOs, MSVC, MinGW and Linux on
+ *       all other platforms/compilers it simply does nothing.
 **/
