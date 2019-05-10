a372461d9321a123f70f6040ec11b80b3f11dc0a
AMS21 <hansfritzenmeister@yahoo.de>
2019-05-10 16:41:07 +0200

    Fix pp checks

--- include/cpp/Assume.hpp
+++ include/cpp/Assume.hpp
@@ -5,8 +5,12 @@
 
 #if CPP_COMPILER_IS(CPP_COMPILER_MSVC)
 #    define CPP_ASSUME(condition) __assume(condition)
-#elif CPP_COMPILER_IS(CPP_COMPILER_CLANG) && __has_builtin(__builtin_assume)
-#    define CPP_ASSUME(condition) __builtin_assume(condition)
+#elif CPP_COMPILER_IS(CPP_COMPILER_CLANG)
+#    if __has_builtin(__builtin_assume)
+#        define CPP_ASSUME(condition) __builtin_assume(condition)
+#    else
+#        define CPP_ASSUME(condition) CPP_BEGIN_MACRO CPP_END_MACRO /* Nothing */
+#    endif
 #else
 #    define CPP_ASSUME(condition) CPP_BEGIN_MACRO CPP_END_MACRO /* Nothing */
 #endif
