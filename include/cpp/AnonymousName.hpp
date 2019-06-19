094637171b5035075aa4f38f47d7e46125ed3a82
AMS21 <hansfritzenmeister@yahoo.de>
2019-06-19 18:14:53 +0200

    Added documentation for CPP_ANONYMOUS_NAME

--- include/cpp/AnonymousName.hpp
+++ include/cpp/AnonymousName.hpp
@@ -1,7 +1,19 @@
+/*!
+ * \file AnonymouseName.hpp
+ * \brief Exports a macro that simplifies the definition functions or variables
+ *        with an anonymous name.
+**/
 #pragma once
 
-#include "Glue.hpp"
+#include "Glue.hpp" // CPP_GLUE
 
+/*!
+ * \def CPP_ANONYMOUS_NAME
+ * \brief Used to wrap something into an anonymous name
+ * \note Due to a limitation on some compilers its recommended, for maximum
+ *        cross platform support, that you do not use this macro multiple times
+ *        on the same line or mess around with the #line pre processor directive.
+**/
 #if defined(__COUNTER__) // not standard and may be missing for some compilers
 #    define CPP_ANONYMOUS_NAME(x) CPP_GLUE(x, __COUNTER__)
 #else // __COUNTER__
